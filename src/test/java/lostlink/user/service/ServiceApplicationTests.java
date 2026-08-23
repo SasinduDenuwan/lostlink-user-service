321aabc231f1573f962f04ad695e16e2883790df
Sasindu Denuwan <sasindudenuwan2006wpsk@gmail.com>
2026-08-23 13:12:19 +0530

    first commit

---
+++ src/test/java/lostlink/user/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lostlink.user.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
